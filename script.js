52e81a0de4426c103e3e1fdb86e8225d78cfa6a6
Dener Dev <itsdener1404@gmail.com>
2025-05-29 08:25:14 -0300

    Adiciona arquivos da calculadora

---
+++ script.js
@@ -0,0 +1,30 @@
+function insert(num) {
+  document.getElementById("display").value += num;
+}
+
+function limpar() {
+  document.getElementById("display").value = "";
+}
+
+function calcular() {
+  try {
+    const resultado = eval(document.getElementById("display").value);
+    document.getElementById("display").value = resultado;
+  } catch (e) {
+    alert("Comando Ínvalido");
+  }
+}
+
+document.addEventListener("keydown", function (e) {
+  const display = document.getElementById("display");
+  const teclasValidas = "0123456789*/.";
+  if (teclasValidas.includes(e.key)) {
+    insert(e.key);
+  } else if (e.key === "Enter") {
+    calcular();
+  } else if (e.key === "Backspace") {
+    display.value = display.value.slice(0, -1);
+  } else if (e.key === "Escape") {
+    limpar();
+  }
+});
