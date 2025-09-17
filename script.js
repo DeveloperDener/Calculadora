d59d8b44c15d9e0703059593fd4dfa52f5294228
DenerCaetano <ds@voeja.us>
2025-09-17 14:21:34 -0300

    refactor: design da calculadora

--- script.js
+++ script.js
@@ -16,6 +16,43 @@ function calcular() {
 }
 
 function ApagarUltimo() {
+  const display = document.getElementById("display");
+  display.value = display.value.slice(0, -1);
+}
+
+document.addEventListener("keydown", function (e) {
+  const display = document.getElementById("display");
+  const teclasValidas = "0123456789*/.";
+  if (teclasValidas.includes(e.key)) {
+    insert(e.key);
+  } else if (e.key === "Enter") {
+    calcular();
+  } else if (e.key === "Backspace") {
+    display.value = display.value.slice(0, -1);
+  } else if (e.key == "←") {
+    display.value = display.value.slice(0, -1);
+  } else if (e.key === "Escape") {
+    limpar();
+  }
+});
+function insert(num) {
+  document.getElementById("display").value += num;
+}
+
+function limpar() {
+  document.getElementById("display").value = "";
+}
+
+function calcular() {
+  try {
+    const resultado = eval(document.getElementById("display").value);
+    document.getElementById("display").value = resultado;
+  } catch (e) {
+    alert("Comando Ínvalido");
+  }
+}
+
+function ApagarUltimo() {
   document.getElementById("display");
   display.value = display.value.slice(0, -1);
 }
